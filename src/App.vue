7e6d7ce6141172ba3d77970b83f9bef7a25a2bfd
daiwanxing <377099119@qq.com>
2021-09-04 18:55:20 +0800

    feat: 提交issue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script lang="tsx">
+import { defineComponent, ref } from "vue";
+import { Switch as ASwitch } from "ant-design-vue"
+
+export default defineComponent({
+  components: {
+    ASwitch
+  },
+  setup () {
+    const checked = ref(false);
+    return function () {
+      return <div>
+        <ASwitch 
+          checked={checked.value}
+          // 此处check的类型被推断成了any
+          onChange={function (check) {
+            checked.value = check;
+          }}
+        ></ASwitch>
+      </div>
+    }
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
+
+createApp(App).mount('#app')
